d5fa8715c10807604f68883f426c1d8da3ae32c3
Adrian Villamor <AdrianVillamor>
2018-11-06 18:20:27 +0800

    "JOB WELL DONE"

--- events/views.py
+++ events/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView
-
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Event
 class HomePageView(TemplateView):
     template_name = "home.html"
 
@@ -10,3 +10,9 @@ class EventListView(ListView):
     model = Event
     template_name= "event_list.html"
     context_object_name ="event_list"
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name= "event_detail.html"
+    context_object_name ="event"
+    
